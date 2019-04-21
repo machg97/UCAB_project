3562a41ef867d40b3d7d0b7868afdc9dd0af40ba
OmarElFakih <omarelfakih97@gmail.com>
2019-04-21 19:44:30 -0400

    New Added Scripts
    Targeted rotationScripts and Mouse Folower Scripts have been added

---
+++ UCABUnity/Assets/_Scripts/MouseFollower.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseFollower : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 pz = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		pz.z = 0;
+		gameObject.transform.position = pz;
+	}
+}
---
+++ UCABUnity/Assets/_Scripts/TargetedRotation.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TargetedRotation : MonoBehaviour
+{
+    
+    public Transform target;
+
+    public float rotationSpeed;
+    public ForwardSide forwardSide;
+   
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vectorToTarget = target.position - transform.position;
+
+        float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg - AngleOffset();
+
+        Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationSpeed);
+    }
+
+    private float AngleOffset()
+    {
+        float AngularOffset = 0f;
+
+        switch (forwardSide)
+        {
+            case ForwardSide.Top:
+                AngularOffset = 90f;
+                break;
+
+            case ForwardSide.Right:
+                AngularOffset = 0f;
+                break;
+
+            case ForwardSide.Buttom:
+                AngularOffset = 270f;
+                break;
+
+            case ForwardSide.Left:
+                AngularOffset = 180f;
+                break;
+
+        }
+
+        return AngularOffset;
+    }
+
+
+
+    public enum ForwardSide
+    {
+        Top,
+        Right,
+        Buttom,
+        Left
+    }
+}
